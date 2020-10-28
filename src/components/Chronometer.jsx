95ab5ce82ad04b0513179725acb573c372909758
Alejandro  Mauricio <abecerraguz@MacBook-Pro-de-Alejandro.local>
2020-10-28 17:41:56 -0300

    Ejercicio de un cronometro con class componentes

---
+++ src/components/Chronometer.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+
+import { nanoid } from 'nanoid'
+
+import styled from 'styled-components'
+
+class Chronometer extends Component {
+
+    state = {
+
+    }
+
+    //Función que se llama con el boton start
+    handleStartClick = () => {
+
+    }
+
+    //Conteo del cronómetro
+    tick() {
+
+    }
+
+    //Función que se llama con el boton stop
+    handleStopClick = () => {
+
+    }
+
+    //Función que se llama con el boton timestamp
+    handleTimestamp = () => {
+
+    }
+
+    //Función que se llama con el boton reset
+    handleReset = () => {
+
+    }
+
+    //Función de actualización del estado
+    updateTimer(miliseconds, seconds, minutes, hours) {
+
+    }
+
+    render() {
+        return (
+            <p>Code</p>
+        )
+    }
+}
+
+export default Chronometer;
